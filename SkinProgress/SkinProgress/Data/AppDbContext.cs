26e4f2fff148acf6a3794d6da9713c887bc5cce2
cosmincreato <marasoiu.cos@gmail.com>
2025-12-29 20:43:31 +0200

    feat(api): database setup

---
+++ SkinProgress/SkinProgress/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkinProgress.Models.Entities;
+
+namespace SkinProgress.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    public DbSet<User> Users { get; set; }
+}
---
+++ SkinProgress/SkinProgress/Models/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkinProgress.Models.Entities;
+
+public class User
+{
+    [Key]
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    [Required]
+    public string Username { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
